9dd9031ff5fd4e089d9f425b19a7a3479078edfd
Pro203S <prosirea2501@gmail.com>
2026-04-26 00:58:53 +0900

    아니 git 설정 잘못건드렸다

--- src/bin/commands/clear-commands.ts
+++ src/bin/commands/clear-commands.ts
@@ -1,15 +1,23 @@
+import chalk from "chalk";
 import { REST, RESTOptions, Routes } from "discord.js"
+import ora from 'ora';
 
 export default async function ClearCommands(options: {
     "token": string,
     "appId": string,
-    "restOptions": Partial<RESTOptions>
+    "restOptions"?: Partial<RESTOptions>
 }) {
     const { token, appId, restOptions } = options;
+
+    const spinner = ora(chalk.gray("Processing with application id " + appId.slice(0, 10) + "..."));
+    spinner.start();
+
     const rest = new REST(restOptions)
         .setToken(token);
 
     await rest.put(Routes.applicationCommands(appId), {
         "body": []
     });
+
+    spinner.succeed("Cleared!");
 }
--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import c from "chalk";
 import * as fs from 'fs';
+import Project from "../modules/project";
+import ClearCommands from "./commands/clear-commands";
 
 const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf-8"));
 
@@ -9,18 +11,23 @@ const helpMessage = () => {
     console.log("Usage: npx discord [command]");
     console.log("Commands:");
     console.log("- start:          Starts the bot.");
-    console.log("- clear-commands: Clears all commands.");
+    console.log("- clear-commands: Clears " + c.bold("all types of application commands."));
 };
 
 (async () => {
     try {
         const command = process.argv[2];
+        const project = new Project(process.cwd());
 
         switch (command) {
             case "start":
 
                 break;
             case "clear-commands":
+                await ClearCommands({
+                    "token": "asdf",
+                    "appId": "123124812958192581"
+                });
                 break;
             default:
                 helpMessage();
